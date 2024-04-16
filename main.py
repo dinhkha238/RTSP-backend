1a0a5060c3ea2d856afa57127170959c8772be97
kha <nguyendinhkha.s4c.@gmail.com>
2024-04-16 14:30:31 +0700

    add funtion stop

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
+import glob
 import os
 import time
 from dotenv import load_dotenv
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Request
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 import uvicorn
 import threading
 from models.main import Rtsp
-from services.main import cleanup_folder, convert_to_hls, cut_ip_address, thread_exists
+from services.main import cleanup_folder, convert_to_hls, cut_ip_address, stop_ffmpeg_by_ip, thread_exists
+from motor.motor_asyncio import AsyncIOMotorClient
+import requests
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
 
 load_dotenv()
 secret_url_api = os.environ.get('SECURITY_URL_API')
+secret_url_mongo_rtsp = os.environ.get('SECURITY_URL_MONGO_RTSP')
+secret_url_api_cxview = os.environ.get('SECURITY_URL_API_CXVIEW')
 
 app = FastAPI()
 
@@ -22,29 +28,90 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# Kết nối tới MongoDB
+client = AsyncIOMotorClient(secret_url_mongo_rtsp)
+db = client["rtsp-camera"]
+collect_camera = db["camera"]
+
+# Tạo thư mục chứa video
 if not os.path.exists('./videos'):
     os.mkdir('./videos')
 app.mount("/videos", StaticFiles(directory="videos"), name="videos")
 
-@app.post("/convert")
-async def start_conversion(rtsp: Rtsp):
-        ip_cam = cut_ip_address(rtsp.rtsp)
-        print("ip_cam",ip_cam)
-        if not thread_exists(ip_cam):
-            cleanup_folder(ip_cam)
-            conversion_thread = threading.Thread(target=convert_to_hls, args=(rtsp.rtsp,""), name=ip_cam)
-            conversion_thread.start()
-        else:
-            print("Thread đã tồn tại, không tạo mới.")
-        while not os.path.exists(os.path.join("videos",ip_cam,"output.m3u8")):
-            time.sleep(1)
-        return {"hls_url": f"{secret_url_api}/videos/{ip_cam}/output.m3u8"}
+# Dictionary to store running processes
+running_processes = {}
 
-@app.get("/active_threads")
-async def active_threads():
+# get
+@app.get("/active-threads")
+def get_active_threads():
     active = threading.enumerate()
     thread_names = [t.name for t in active]
     return {"active_threads": thread_names}
+
+@app.get("/list-process")
+def get_list_process():
+    if running_processes:
+        print("Running Processes:")
+        for rtsp_url, process in running_processes.items():
+            print(f"  RTSP URL: {rtsp_url}, PID: {process.pid}")
+    else:
+        print("No running processes.")
+
+@app.get("/list-rtsp")
+async def get_list_rtsp(request: Request):
+    token = request.headers.get("Authorization")
+    requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+    data_user = requests.get(f"{secret_url_api_cxview}/prod/api/v1/user-management/get-profile", headers={"Authorization": token},verify=False)
+    data_rtsp = await collect_camera.find_one({"email": data_user.json()["data"]["email"]})
+    if data_rtsp == None:
+        return {"data":{}}
+    return {"data":data_rtsp}
+
+# post
+@app.post("/stop-ffmpeg")
+def post_stop_ffmpeg(rtsp: Rtsp):
+    ip_cam = cut_ip_address(rtsp.rtsp)
+    stop_ffmpeg_by_ip(ip_cam,running_processes)
+
+@app.post("/convert")
+def post_start_conversion(rtsp: Rtsp):
+        ip_cam = cut_ip_address(rtsp.rtsp)
+        if not thread_exists(ip_cam, running_processes):
+            cleanup_folder(ip_cam)
+            conversion_thread = threading.Thread(target=convert_to_hls, args=(rtsp.rtsp,running_processes), name=ip_cam)
+            conversion_thread.start()
+        else:
+            print("Thread đã tồn tại, không tạo mới.")
+        while not glob.glob(os.path.join("videos", ip_cam, "*.ts")):
+            time.sleep(1)  # Đợi 1 giây trước khi kiểm tra lại
+        return {"hls_url": f"{secret_url_api}/videos/{ip_cam}/output.m3u8"}
+
+@app.post("/add-rtsp")
+async def post_add_rtsp(rtsp: Rtsp, request: Request):
+    token = request.headers.get("Authorization")
+    requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+    data_user = requests.get(f"{secret_url_api_cxview}/prod/api/v1/user-management/get-profile", headers={"Authorization": token},verify=False)
+    data_rtsp = await collect_camera.find_one({"email": data_user.json()["data"]["email"]})
+    if data_rtsp == None:
+        data_rtsp = {
+            "email": data_user.json()["data"]["email"],
+            "rtsp": [rtsp.rtsp]
+        }
+        await collect_camera.insert_one(data_rtsp)
+    else:
+        for rtsp_item in data_rtsp["rtsp"]:
+            if rtsp_item["link"] == rtsp.rtsp:
+                raise HTTPException(status_code=400, detail="RTSP đã tồn tại.")
+        rtsp_new = {
+            "id": len(data_rtsp["rtsp"]) + 1,
+            "link": rtsp.rtsp,
+            "status": False,
+            "image_lastest": "https://dfstudio-d420.kxcdn.com/wordpress/wp-content/uploads/2019/06/digital_camera_photo-1080x675.jpg"
+        }
+        data_rtsp["rtsp"].append(rtsp_new)
+        await collect_camera.update_one({"email": data_user.json()["data"]["email"]}, {"$set": data_rtsp})
+    return {"message": "Thêm RTSP thành công."}
+        
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
--- services/main.py
+++ services/main.py
@@ -1,6 +1,6 @@
 import os
-import threading
 import subprocess
+import cv2
 
 def cut_ip_address(url):
     start_index = url.find('@') + 1
@@ -9,32 +9,36 @@ def cut_ip_address(url):
     ip_address = ip_address.replace(":", "_")
     return ip_address
 
-def convert_to_hls(rtsp_url: str, _: str):
-    if not os.path.exists(os.path.join("videos",cut_ip_address(rtsp_url))):
-        os.makedirs(os.path.join("videos",cut_ip_address(rtsp_url)))
-    output_m3u8 = os.path.join("videos",cut_ip_address(rtsp_url), "output.m3u8")
-    subprocess.run([
-    "ffmpeg",
-    "-rtsp_transport", "tcp",
-    "-i", rtsp_url,
-    "-c:v", "copy",
-    "-c:a", "aac",
-    "-tag:v", "hvc1",  # Xác định tag:v là 'hvc1' cho codec HEVC
-    "-f", "hls",
-    "-hls_time", "4",  # Thay đổi từ 7 giây thành 2 giây
-    "-hls_list_size", "4",
-    "-hls_flags", "delete_segments",
-    "-max_muxing_queue_size", "1024",
-    "-start_number", "1",  # Thêm tham số để bắt đầu với số phân đoạn là 1
-    output_m3u8
-], check=True)
+def convert_to_hls(rtsp_url: str, running_processes):
+    if check_rtsp_online(rtsp_url):
+        print("RTSP is online.")
+        if not os.path.exists(os.path.join("videos",cut_ip_address(rtsp_url))):
+            os.makedirs(os.path.join("videos",cut_ip_address(rtsp_url)))
+        output_m3u8 = os.path.join("videos",cut_ip_address(rtsp_url), "output.m3u8")
+        process = subprocess.Popen([
+            "ffmpeg",
+            "-rtsp_transport", "tcp",
+            "-i", rtsp_url,
+            "-c:v", "copy",
+            "-c:a", "aac",
+            "-tag:v", "hvc1",  
+            "-f", "hls",
+            "-hls_time", "4",  
+            "-hls_list_size", "4",
+            "-hls_flags", "delete_segments",
+            "-max_muxing_queue_size", "1024",
+            "-start_number", "1",  
+            output_m3u8
+        ])
+        running_processes[cut_ip_address(rtsp_url)] = process
+    else:
+        print("RTSP is offline.")
 
-def thread_exists(thread_name):
-    print(thread_name)
-    for thread in threading.enumerate():
-        if thread.name == thread_name:
-            return True
+def thread_exists(thread_name,running_processes):
+    if thread_name in running_processes:
+        return True
     return False
+    
 
 def cleanup_folder(folder_path):
     if not os.path.exists(os.path.join("videos",folder_path)):
@@ -47,3 +51,21 @@ def cleanup_folder(folder_path):
                 os.remove(file_path)  # Sử dụng os.remove để xóa tệp
         except Exception as e:
             print(f"Failed to delete {file_path}: {e}")
+
+# Function to stop a running process
+def stop_ffmpeg_by_ip(ip_cam,running_processes):
+    if ip_cam in running_processes:
+        process = running_processes[ip_cam]
+        process.terminate()
+        del running_processes[ip_cam]
+        cleanup_folder(ip_cam)
+        return {"message": f"Process for {ip_cam} has been terminated."}
+    else:
+        return {"message": f"No process found for {ip_cam}."}
+
+def check_rtsp_online(rtsp_url):
+    cap = cv2.VideoCapture(rtsp_url)
+    if not cap.isOpened():
+        return False
+    else:
+        return True
